b1b8b508232a09d663d166bf81b03a91efd01f42
TaoweiLi <104708757+TaoweiLi@users.noreply.github.com>
2022-09-19 14:36:13 -0700

    finish task model

---
+++ models/Task.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const taskSchema = Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: "User"
+        },
+        project: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: "Project"
+        },
+        title: {
+            type: String,
+            require: [true, 'Please add a title']
+        },
+        body: {
+            type: Text,
+        },
+        deadline: {
+            type: Date,
+            require: [true, 'Please add a deadline']
+        },
+        priority: {
+            type: Integer,
+            require: [true, 'Please select priority']
+        },
+    }
+)
+
+module.export = mongoose.model("Task", taskSchema);
