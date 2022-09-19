5b892e7bec9c188bd2ce5a445805c2b9461efa7e
TaoweiLi <104708757+TaoweiLi@users.noreply.github.com>
2022-09-19 14:20:53 -0700

    finish project schema

---
+++ models/Project.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const projectSchema = Schema(
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            ref: "User"
+        },
+        title: {
+            type: String,
+            required: [true, 'Please add a title']
+        },
+        description: {
+            type: Text
+        },
+        deadline: {
+            type: Date,
+            require: [true, 'Please add a deadline']
+        },
+        member: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    }, 
+    {
+        timestamps: true
+    }
+)
+
+projectSchema.index({name: 1, title: 1}) //mongoose 
+
+module.export = mongoose.model("Project", projectSchema);
