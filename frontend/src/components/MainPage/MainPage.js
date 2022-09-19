d2f31feb001c0f1d5ed6cfdf83b52e0310161595
bob-skywalker <q463250938@gmail.com>
2022-09-19 13:36:31 -0700

    complete phase 4 user-auth frontend

---
+++ frontend/src/components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+function MainPage(){
+    return(
+        <>
+        <p>Project Sticky</p>
+        <footer>
+            Copyright &copy; 2022 Project Sticky
+        </footer>
+        </>
+    )
+}
+
+export default MainPage;
