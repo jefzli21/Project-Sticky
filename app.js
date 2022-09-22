17940ff6c530a8672592a2c7af272cd17edf39e0
jefzli21 <jefzli21@gmail.com>
2022-09-22 12:16:58 -0700

    heroku

--- app.js
+++ app.js
@@ -59,6 +59,30 @@ app.use((req, res, next) => {
   next(err);
 });
 
+// Serve static React build files statically in production
+if (isProduction) {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  app.get('/', (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, './frontend', 'build', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  app.use(express.static(path.resolve("./frontend/build")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  app.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('CSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, './frontend', 'build', 'index.html')
+    );
+  });
+}
+//
+
 const serverErrorLogger = debug('backend:error');
 // Express custom error handler that will be called whenever a route handler or
 // middleware throws an error or invokes the `next` function with a truthy value
